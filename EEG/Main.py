79acac9e0b72899b4c6066c1e4aa8c03c203d68e
AbdEl-Rhman El-Amin <a.elamin95@gmail.com>
2017-10-10 17:20:31 +0200

    Tested on 1s and 3s with accuracy 100% with LinearSVM , and 80% with GaussianNB

--- EEG/Main.py
+++ EEG/Main.py
@@ -3,7 +3,7 @@ import Preprocessing
 import pickle
 from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
-import matplotlib.pyplot as plt
+import matplotestlabels_otib.pyplot as plt
 import warnings
 from sklearn.preprocessing import RobustScaler
 import math
@@ -25,10 +25,10 @@ for i in range(1,32):
 
     for k in t:
         d+=Preprocessing.getData([f["data"][k]])
-        l+=Preprocessing.getLabels([f["labels"][k]])
+        l+=Preprocessing.getestlabels_otabels([f["labels"][k]])
     for k in r:
         test += Preprocessing.getData([f["data"][k]])
-        s += Preprocessing.getLabels([f["labels"][k]])
+        s += Preprocessing.getestlabels_otabels([f["labels"][k]])
     clf = GaussianNB()
     clf.fit(np.array(d), np.array(l))
     c =0
@@ -39,28 +39,52 @@ for i in range(1,32):
             c+=1
     print(i," : ",c, "Correct out of ", len(test)," Percesion: " ,c/len(test)*100 )'''
 
-for i in range(1,32):
-    if i<10:
+for i in range(1,2):
+    '''if i<10:
         file = open("D:/Boody/CESS/GP/EEG/data_preprocessed_python/s0"+str(i)+".dat", "rb")
     else:
-        file = open("D:/Boody/CESS/GP/EEG/data_preprocessed_python/s" +str(i) + ".dat", "rb")
+        file = open("D:/Boody/CESS/GP/EEG/data_preprocessed_python/s" +str(i) + ".dat", "rb")'''
+    file = open("D:/Boody/CESS/GP/EEG/data_preprocessed_python/s01.dat", "rb")
+
     f = pickle.load(file, encoding="latin1")
     file.close()
-    data = Preprocessing.getData(f["data"][:20])+Preprocessing.getData(f["data"][30:])
-    labels = Preprocessing.getLabels(f["labels"][0:20])+Preprocessing.getLabels(f["labels"][30:])
 
     s=svm.LinearSVC()
-    #clf = GaussianNB()
-    #clf.fit(np.array(data), np.array(labels))
-    s.fit(np.array(data), np.array(labels))
+    #s = GaussianNB()
+    data = Preprocessing.getData(f["data"][:20]) + Preprocessing.getData(f["data"][30:])
+    labels = Preprocessing.getestlabels_otabels(f["labels"][0:20]) + Preprocessing.getestlabels_otabels(f["labels"][30:])
+    # test = Preprocessing.getData(f["data"][20:30])
+    # correct = Preprocessing.getestlabels_otabels(f["labels"][20:30])
 
-    test = Preprocessing.getData(f["data"][20:30])
-    correct = Preprocessing.getLabels(f["labels"][20:30])
+    data_ot = []
+    labels_ot = []
+    test_ot=[]
+    testlabels_ot=[]
+    ones=0
+    threes=0
+    for i in range(len(labels)):
+        if labels[i]==1 :
+            if (ones < 5):
+                data_ot.append(data[i])
+                labels_ot.append(labels[i])
+            else:
+                test_ot.append(data[i])
+                testlabels_ot.append(labels[i])
+            ones+=1
+        elif labels[i]==3:
+            if(threes<5):
+                data_ot.append(data[i])
+                labels_ot.append(labels[i])
+            else:
+                test_ot.append(data[i])
+                testlabels_ot.append(labels[i])
+            threes+=1
+    s.fit(np.array(data_ot), np.array(labels_ot))
 
     c =0
-    for k in range(len(test)):
-        pr = s.predict(np.array(test[k]))
-        print("Predicted: ", pr[0], " ,Correct Value: ", correct[k] )
-        if pr[0] == correct[k]:
+    for k in range(len(test_ot)):
+        pr = s.predict(np.array(test_ot[k]))
+        print("Predicted: ", pr[0], " ,Correct Value: ", testlabels_ot[k] )
+        if pr[0] == testlabels_ot[k]:
             c+=1
-    print(i," : ",c, "Correct out of ", len(test)," Percesion: " ,c/len(test)*100 )
+    print(i," : ",c, "Correct out of ", len(testlabels_ot)," Percesion: " ,c/len(testlabels_ot)*100 )
