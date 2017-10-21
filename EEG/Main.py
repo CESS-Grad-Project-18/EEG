ed3298a12df1d3855b207514a56d407ca0106f45
omargamal8 <omargamalh@hotmail.com>
2017-10-21 02:58:50 +0200

    Unfinished work on SVM paramaters

--- EEG/Main.py
+++ EEG/Main.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 import warnings
 from sklearn.preprocessing import RobustScaler
 import math
-from mlxtend.plotting import plot_decision_regions
+# from mlxtend.plotting import plot_decision_regions
+
 warnings.filterwarnings("ignore")
 
 data_ot = []
@@ -18,7 +19,11 @@ ones=0
 twos=0
 threes=0
 fours=0
-for i in range(1,33):
+
+first_file_index = 1
+last_file_index =  10
+print("Using",last_file_index - first_file_index + 1, "file(s)")
+for i in range(first_file_index, last_file_index + 1):
     if i<10:
         file = open("./"+"s0"+str(i)+".dat", "rb")
     else:
@@ -27,13 +32,19 @@ for i in range(1,33):
     f = pickle.load(file, encoding="latin1")
     file.close()
 
-    s=svm.LinearSVC()
-    #s = GaussianNB()
+    # s=svm.LinearSVC()
+    # s = svm.SVC()
+    # s = svm.SVR()
+    # s = svm.SVC(kernel = "rbf", class_weight = {1:0.15})
+    # s = svm.SVC(C=1.0, tol=1e-10, cache_size=600, kernel='rbf', gamma = 1e-6)
+    # s = svm.SVC(C=1.0, tol=1e-10, cache_size=600, kernel='poly', gamma = 1e-6)
+    s = svm.SVC(kernel = "poly") 
+    # s = GaussianNB()
     data = Preprocessing.getData(f["data"])
     labels = Preprocessing.getLabels(f["labels"])
 
 
-    label_filter = [1,3]
+    label_filter = [1,2,3,4]
     for d, label in zip(data,labels):
     	if label in label_filter:
     		data_ot.append(d)
@@ -46,7 +57,10 @@ for i in range(1,33):
     testing_labels = np.asarray( labels_ot[len(features):])
 
     ones = len( np.where(np.asarray(labels_ot) == 1)[0] )
-    threes = len(labels_ot) - ones
+    twos = len( np.where(np.asarray(labels_ot) == 2)[0] )
+    threes = len( np.where(np.asarray(labels_ot) == 3)[0] )
+    fours = len( np.where(np.asarray(labels_ot) == 4)[0] )
+    # threes = len(labels_ot) - ones
 
 
 print("Training data : ",len(features), len(labels))
@@ -54,7 +68,14 @@ print("Testing Data : ",len(testing_features), len(testing_labels))
 print("Total : ", ones + threes)
 print("Ones : ",ones," Twos : ",twos," Threes : ",threes," Fours : ",fours)
 print("Ones in training data : ", len(np.where(labels == 1)[0])  )
-print("Threes in training data : ", len(np.where(testing_labels == 3)[0])  )
+print("Twos in training data : ", len(np.where(labels == 2)[0])  )
+print("Threes in training data : ", len(np.where(labels == 3)[0])  )
+print("fours in training data : ", len(np.where(labels == 4)[0])  )
+
+print("Ones in testing data : ", len(np.where(testing_labels == 1)[0])  )
+print("Twos in testing data : ", len(np.where(testing_labels == 2)[0])  )
+print("Threes in testing data : ", len(np.where(testing_labels == 3)[0])  )
+print("fours in testing data : ", len(np.where(testing_labels == 4)[0])  )
 
 s.fit(features, labels)
 
