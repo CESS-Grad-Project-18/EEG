6558a9f71ab8e72576344948bc76b0802a0ec5d7
Osama Mohamed Mostafa AbdElMageid Ghozlan <osama.ghozlan@gmail.com>
2018-02-22 13:59:20 +0200

    Updated Com.h. Added all function prototypes

--- AUTOSAR/COM/Com.h
+++ AUTOSAR/COM/Com.h
@@ -16,22 +16,54 @@
 #include "Com_Com.h"
 #include "Com_Sched.h"*/
 
+
+/* Startup and Control Services*/
 void Com_Init(const Com_ConfigType * config); /*SID 0x01*/
 void Com_DeInit(void); /*SID 0x02*/
 void Com_IpduGroupControl(Com_IpduGroupVector ipduGroupVector, boolean initialize); /*SID 0x03*/
 void Com_ReceptionDMControl(Com_IpduGroupVector ipduGroupVector); /*SID 0x06*/
 Com_StatusType Com_GetStatus(void) /*SID 0x07, returns COM_INIT or COM_UNINIT*/
 void Com_GetVersionInfo(Std_VersionInfoType* versioninfo) /*SID 0x09, Version out*/
-void Com_IpduGroupStart(Com_PduGroupIdType IpduGroupId, boolean Initialize);
-void Com_IpduGroupStop(Com_PduGroupIdType IpduGroupId);
+void Com_ClearIpduGroupVector(Com_IpduGroupVector ipduGroupVector); /*SID 0x1c*/
+void Com_SetIpduGroup(Com_IpduGroupVector ipduGroupVector, Com_IpduGroupIdType ipduGroupId, boolean bitval); /*SID 0x1d */
 
 
-BufReq_ReturnType Com_CopyTxData(PduIdType PduId, PduInfoType* PduInfoPtr, RetryInfoType* RetryInfoPtr, PduLengthType* TxDataCntPtr);
+
+/* Communication Services */
+uint8 Com_SendSignal(Com_SignalIdType SignalId, const void* SignalDataPtr); /*SID 0x0a*/
+uint8 Com_SendDynSignal(Com_SignalIdType SignalId, const void* SignalDataPtr, uint16 Length); /*SID 0x21*/
+uint8 Com_ReceiveSignal(Com_SignalIdType SignalId, void* SignalDataPtr); /*SID 0x0b*/
+uint8 Com_ReceiveDynSignal(Com_SignalIdType SignalId, void* SignalDataPtr, uint16* Length); /*SID 0x22*/
+void Com_UpdateShadowSignal(Com_SignalIdType SignalId, const void* SignalDataPtr); /*SID 0x0c*/
+uint8 Com_SendSignalGroup(Com_SignalGroupIdType SignalGroupId); /*SID 0x0d */
+uint8 Com_ReceiveSignalGroup(Com_SignalGroupIdType SignalGroupId); /*SID 0x0e*/
+void Com_ReceiveShadowSignal(Com_SignalIdType SignalId, void* SignalDataPtr); /*SID 0x0f*/
+uint8 Com_SendSignalGroupArray(Com_SignalGroupIdType SignalGroupId, const uint8* SignalGroupArrayPtr); /*SID 0x23*/
+uint8 Com_ReceiveSignalGroupArray(Com_SignalGroupIdType SignalGroupId, uint8* SignalGroupArrayPtr); /*SID 0x24*/
+uint8 Com_InvalidateSignal(Com_SignalIdType SignalId) /*SID 0x10*/
+void Com_InvalidateShadowSignal(Com_SignalIdType SignalId) /*SID 0x16*/
+uint8 Com_InvalidateSignalGroup(Com_SignalGroupIdType SignalGroupId); /*SID 0x1b*/
+Std_ReturnType Com_TriggerIPDUSend(PduIdType PduId); /*SID 0x17*/
+Std_ReturnType Com_TriggerIPDUSendWithMetaData(PduIdType PduId, uint8* MetaData); /*SID 0x28*/
+void Com_SwitchIpduTxMode(PduIdType PduId, boolean Mode); /*SID 0x27*/
+
+
+/* Callback functions and Notifications */
+Std_ReturnType Com_TriggerTransmit(PduIdType TxPduId, PduInfoType* PduInfoPtr); /*SID 0x41*/
+void Com_RxIndication(PduIdType RxPduId, const PduInfoType* PduInfoPtr); /*SID 0x42*/
+void Com_TpRxIndication(PduIdType id, Std_ReturnType result); /*SID 0x45*/
+void Com_TxConfirmation(PduIdType TxPduId); /*SID 0x40*/
+void Com_TpTxConfirmation(PduIdType id, Std_ReturnType result); /*SID 0x48*/
+BufReq_ReturnType Com_StartOfReception(PduIdType id, const PduInfoType* info,PduLengthType TpSduLength, PduLengthType* bufferSizePtr); /*SID 0x46*/
+BufReq_ReturnType Com_CopyRxData(PduIdType id, const PduInfoType* info, PduLengthType* bufferSizePtr); /*SID 0x44*/
 BufReq_ReturnType Com_CopyRxData(PduIdType PduId, const PduInfoType* PduInfoPtr, PduLengthType* RxBufferSizePtr);
-BufReq_ReturnType Com_StartOfReception(PduIdType ComRxPduId, PduLengthType TpSduLength, PduLengthType* RxBufferSizePtr);
-void Com_TpRxIndication(PduIdType PduId, NotifResultType Result);
-void Com_TpTxConfirmation(PduIdType PduId, NotifResultType Result);
+BufReq_ReturnType Com_CopyTxData(PduIdType id, const PduInfoType* info, RetryInfoType* retry, PduLengthType* availableDataPtr); /*SID 0x43*/
 
+
+/* Scheduled Functions*/
+void Com_MainFunctionRx(void); /*SID 0x18*/
+void Com_MainFunctionTx(void); /*SID 0x19*/
+void Com_MainFunctionRouteSignals(void); /*SID 0x1a*/
 
 extern ComSignalEndianess_type Com_SystemEndianness;
 
