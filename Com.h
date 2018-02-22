28186ea321bf78de47fa22c6e9b1ce28dcf33ffb
OMGhozlan <osama.ghozlan@gmail.com>
2018-02-22 14:42:52 +0200

    Redistributed function prototypes in header files

--- Com.h
+++ Com.h
@@ -48,22 +48,6 @@ Std_ReturnType Com_TriggerIPDUSendWithMetaData(PduIdType PduId, uint8* MetaData)
 void Com_SwitchIpduTxMode(PduIdType PduId, boolean Mode); /*SID 0x27*/
 
 
-/* Callback functions and Notifications */
-Std_ReturnType Com_TriggerTransmit(PduIdType TxPduId, PduInfoType* PduInfoPtr); /*SID 0x41*/
-void Com_RxIndication(PduIdType RxPduId, const PduInfoType* PduInfoPtr); /*SID 0x42*/
-void Com_TpRxIndication(PduIdType id, Std_ReturnType result); /*SID 0x45*/
-void Com_TxConfirmation(PduIdType TxPduId); /*SID 0x40*/
-void Com_TpTxConfirmation(PduIdType id, Std_ReturnType result); /*SID 0x48*/
-BufReq_ReturnType Com_StartOfReception(PduIdType id, const PduInfoType* info,PduLengthType TpSduLength, PduLengthType* bufferSizePtr); /*SID 0x46*/
-BufReq_ReturnType Com_CopyRxData(PduIdType id, const PduInfoType* info, PduLengthType* bufferSizePtr); /*SID 0x44*/
-BufReq_ReturnType Com_CopyRxData(PduIdType PduId, const PduInfoType* PduInfoPtr, PduLengthType* RxBufferSizePtr);
-BufReq_ReturnType Com_CopyTxData(PduIdType id, const PduInfoType* info, RetryInfoType* retry, PduLengthType* availableDataPtr); /*SID 0x43*/
-
-
-/* Scheduled Functions*/
-void Com_MainFunctionRx(void); /*SID 0x18*/
-void Com_MainFunctionTx(void); /*SID 0x19*/
-void Com_MainFunctionRouteSignals(void); /*SID 0x1a*/
 
 extern ComSignalEndianess_type Com_SystemEndianness;
 
--- SchM_Com.h
+++ SchM_Com.h
@@ -1,4 +1,9 @@
 #ifndef SCHM_COM_H_
 #define SCHM_COM_H_
 
-#elif
+/* Scheduled Functions*/
+void Com_MainFunctionRx(void); /*SID 0x18*/
+void Com_MainFunctionTx(void); /*SID 0x19*/
+void Com_MainFunctionRouteSignals(void); /*SID 0x1a*/
+
+#endif
