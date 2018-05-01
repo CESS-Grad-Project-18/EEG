0a2891fb9d7cab2ccc428f65d984f7305127983f
Osama Mohamed Mostafa AbdElMageid Ghozlan <osama.ghozlan@gmail.com>
2018-05-01 02:01:04 +0200

    Serial communication parameters configured

--- TivaUARTComm.c
+++ TivaUARTComm.c
@@ -3,44 +3,43 @@
 #define BS 0x08 /* Backspace */
 
 /* CAN 0 */
-#define CAN0_CTL_R              (*((volatile unsigned long *)0x40040000))
-#define CAN0_STS_R              (*((volatile unsigned long *)0x40040004))
-#define CAN0_ERR_R              (*((volatile unsigned long *)0x40040008))
-#define CAN0_BIT_R              (*((volatile unsigned long *)0x4004000C))
-#define CAN0_INT_R              (*((volatile unsigned long *)0x40040010))
-#define CAN0_TST_R              (*((volatile unsigned long *)0x40040014))
-#define CAN0_BRPE_R             (*((volatile unsigned long *)0x40040018))
-#define CAN0_IF1CRQ_R           (*((volatile unsigned long *)0x40040020))
-#define CAN0_IF1CMSK_R          (*((volatile unsigned long *)0x40040024))
-#define CAN0_IF1MSK1_R          (*((volatile unsigned long *)0x40040028))
-#define CAN0_IF1MSK2_R          (*((volatile unsigned long *)0x4004002C))
-#define CAN0_IF1ARB1_R          (*((volatile unsigned long *)0x40040030))
-#define CAN0_IF1ARB2_R          (*((volatile unsigned long *)0x40040034))
-#define CAN0_IF1MCTL_R          (*((volatile unsigned long *)0x40040038))
-#define CAN0_IF1DA1_R           (*((volatile unsigned long *)0x4004003C))
-#define CAN0_IF1DA2_R           (*((volatile unsigned long *)0x40040040))
-#define CAN0_IF1DB1_R           (*((volatile unsigned long *)0x40040044))
-#define CAN0_IF1DB2_R           (*((volatile unsigned long *)0x40040048))
-#define CAN0_IF2CRQ_R           (*((volatile unsigned long *)0x40040080))
-#define CAN0_IF2CMSK_R          (*((volatile unsigned long *)0x40040084))
-#define CAN0_IF2MSK1_R          (*((volatile unsigned long *)0x40040088))
-#define CAN0_IF2MSK2_R          (*((volatile unsigned long *)0x4004008C))
-#define CAN0_IF2ARB1_R          (*((volatile unsigned long *)0x40040090))
-#define CAN0_IF2ARB2_R          (*((volatile unsigned long *)0x40040094))
-#define CAN0_IF2MCTL_R          (*((volatile unsigned long *)0x40040098))
-#define CAN0_IF2DA1_R           (*((volatile unsigned long *)0x4004009C))
-#define CAN0_IF2DA2_R           (*((volatile unsigned long *)0x400400A0))
-#define CAN0_IF2DB1_R           (*((volatile unsigned long *)0x400400A4))
-#define CAN0_IF2DB2_R           (*((volatile unsigned long *)0x400400A8))
-#define CAN0_TXRQ1_R            (*((volatile unsigned long *)0x40040100))
-#define CAN0_TXRQ2_R            (*((volatile unsigned long *)0x40040104))
-#define CAN0_NWDA1_R            (*((volatile unsigned long *)0x40040120))
-#define CAN0_NWDA2_R            (*((volatile unsigned long *)0x40040124))
-#define CAN0_MSG1INT_R          (*((volatile unsigned long *)0x40040140))
-#define CAN0_MSG2INT_R          (*((volatile unsigned long *)0x40040144))
-#define CAN0_MSG1VAL_R          (*((volatile unsigned long *)0x40040160))
-#define CAN0_MSG2VAL_R          (*((volatile unsigned long *)0x40040164))
-
+#define CAN0_CTL_R              (*((volatile unsigned long *) 0x40040000))
+#define CAN0_STS_R              (*((volatile unsigned long *) 0x40040004))
+#define CAN0_ERR_R              (*((volatile unsigned long *) 0x40040008))
+#define CAN0_BIT_R              (*((volatile unsigned long *) 0x4004000C))
+#define CAN0_INT_R              (*((volatile unsigned long *) 0x40040010))
+#define CAN0_TST_R              (*((volatile unsigned long *) 0x40040014))
+#define CAN0_BRPE_R             (*((volatile unsigned long *) 0x40040018))
+#define CAN0_IF1CRQ_R           (*((volatile unsigned long *) 0x40040020))
+#define CAN0_IF1CMSK_R          (*((volatile unsigned long *) 0x40040024))
+#define CAN0_IF1MSK1_R          (*((volatile unsigned long *) 0x40040028))
+#define CAN0_IF1MSK2_R          (*((volatile unsigned long *) 0x4004002C))
+#define CAN0_IF1ARB1_R          (*((volatile unsigned long *) 0x40040030))
+#define CAN0_IF1ARB2_R          (*((volatile unsigned long *) 0x40040034))
+#define CAN0_IF1MCTL_R          (*((volatile unsigned long *) 0x40040038))
+#define CAN0_IF1DA1_R           (*((volatile unsigned long *) 0x4004003C))
+#define CAN0_IF1DA2_R           (*((volatile unsigned long *) 0x40040040))
+#define CAN0_IF1DB1_R           (*((volatile unsigned long *) 0x40040044))
+#define CAN0_IF1DB2_R           (*((volatile unsigned long *) 0x40040048))
+#define CAN0_IF2CRQ_R           (*((volatile unsigned long *) 0x40040080))
+#define CAN0_IF2CMSK_R          (*((volatile unsigned long *) 0x40040084))
+#define CAN0_IF2MSK1_R          (*((volatile unsigned long *) 0x40040088))
+#define CAN0_IF2MSK2_R          (*((volatile unsigned long *) 0x4004008C))
+#define CAN0_IF2ARB1_R          (*((volatile unsigned long *) 0x40040090))
+#define CAN0_IF2ARB2_R          (*((volatile unsigned long *) 0x40040094))
+#define CAN0_IF2MCTL_R          (*((volatile unsigned long *) 0x40040098))
+#define CAN0_IF2DA1_R           (*((volatile unsigned long *) 0x4004009C))
+#define CAN0_IF2DA2_R           (*((volatile unsigned long *) 0x400400A0))
+#define CAN0_IF2DB1_R           (*((volatile unsigned long *) 0x400400A4))
+#define CAN0_IF2DB2_R           (*((volatile unsigned long *) 0x400400A8))
+#define CAN0_TXRQ1_R            (*((volatile unsigned long *) 0x40040100))
+#define CAN0_TXRQ2_R            (*((volatile unsigned long *) 0x40040104))
+#define CAN0_NWDA1_R            (*((volatile unsigned long *) 0x40040120))
+#define CAN0_NWDA2_R            (*((volatile unsigned long *) 0x40040124))
+#define CAN0_MSG1INT_R          (*((volatile unsigned long *) 0x40040140))
+#define CAN0_MSG2INT_R          (*((volatile unsigned long *) 0x40040144))
+#define CAN0_MSG1VAL_R          (*((volatile unsigned long *) 0x40040160))
+#define CAN0_MSG2VAL_R          (*((volatile unsigned long *) 0x40040164))
 
 /* Run Mode Clock Gating Control (UART) */
 #define SYSCTL_RCGCUART_R   (*((volatile unsigned long *) 0x400FE618))
@@ -48,13 +47,16 @@
 /* Run Mode Clock Gating Control Register 2 (GPIO) */
 #define SYSCTL_R_RCGC2_R        (*((volatile unsigned long *) 0x400FE108))
 
+#define SYSCTL_RCGCGPIO_R       (*((volatile unsigned long *) 0x400FE608))
+
 /* UART0 */
-#define UART0_CTL_R             (*((volatile unsigned long *)0x4000C030))
-#define UART0_IBRD_R            (*((volatile unsigned long *)0x4000C024))
-#define UART0_FBRD_R            (*((volatile unsigned long *)0x4000C028))
-#define UART0_LCRH_R            (*((volatile unsigned long *)0x4000C02C))
-#define UART0_FR_R              (*((volatile unsigned long *)0x4000C018))
-#define UART0_DR_R              (*((volatile unsigned long *)0x4000C000))
+#define UART0_CTL_R             (*((volatile unsigned long *) 0x4000C030))
+#define UART0_IBRD_R            (*((volatile unsigned long *) 0x4000C024))
+#define UART0_FBRD_R            (*((volatile unsigned long *) 0x4000C028))
+#define UART0_LCRH_R            (*((volatile unsigned long *) 0x4000C02C))
+#define UART0_FR_R              (*((volatile unsigned long *) 0x4000C018))
+#define UART0_DR_R              (*((volatile unsigned long *) 0x4000C000))
+#define UART0_CC_R              (*((volatile unsigned long *) 0x4000CFC8))
 
 /* UART5 */
 #define UART5_CTL_R                 (*((volatile unsigned long *) 0x40011030))
@@ -68,11 +70,17 @@
 #define UART_FR_RXFE                0x00000010
 #define UART_FR_TXFF                0x00000020
 
-/* GPIO */
+/* GPIO Port E*/
 #define GPIO_PORTE_AMSEL_R  (*((volatile unsigned long *)0x40024528))
 #define GPIO_PORTE_PCTL_R   (*((volatile unsigned long *)0x4002452C))
 #define GPIO_PORTE_AFSEL_R  (*((volatile unsigned long *)0x40024420))
 #define GPIO_PORTE_DEN_R        (*((volatile unsigned long *)0x4002451C))
+
+/* GPIO Port A*/
+#define GPIO_PORTA_AMSEL_R      (*((volatile unsigned long *)0x40004528))
+#define GPIO_PORTA_PCTL_R       (*((volatile unsigned long *)0x4000452C))
+#define GPIO_PORTA_AFSEL_R      (*((volatile unsigned long *)0x40004420))
+#define GPIO_PORTA_DEN_R        (*((volatile unsigned long *)0x4000451C))
 
 #define STCTL_R             (*((volatile unsigned long *)0xE000E010))
 #define STRELOAD_R      (*((volatile unsigned long *)0xE000E014))
@@ -89,13 +97,13 @@
 /* U5Tx (transmit) connected to PE5 */
 
 /* PortF */
-#define GPIO_PORTF_DATA_R           (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_AMSEL_R          (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R           (*((volatile unsigned long *)0x4002552C))
-#define GPIO_PORTF_DIR_R            (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_DEN_R            (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_AFSEL_R          (*((volatile unsigned long *)0x40025420))
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+#define GPIO_PORTF_DATA_R           (*((volatile unsigned long *) 0x400253FC))
+#define GPIO_PORTF_AMSEL_R          (*((volatile unsigned long *) 0x40025528))
+#define GPIO_PORTF_PCTL_R           (*((volatile unsigned long *) 0x4002552C))
+#define GPIO_PORTF_DIR_R            (*((volatile unsigned long *) 0x40025400))
+#define GPIO_PORTF_DEN_R            (*((volatile unsigned long *) 0x4002551C))
+#define GPIO_PORTF_AFSEL_R          (*((volatile unsigned long *) 0x40025420))
+#define SYSCTL_RCGC2_R              (*((volatile unsigned long *) 0x400FE108))
 #define SYSCTL_RCGC2_GPIOF          0x00000020
 
 void PLL_Init_50MHz(void) {
@@ -111,19 +119,21 @@ void PLL_Init_50MHz(void) {
 }
 
 void UART0_Init(void){
-  SYSCTL_RCGCUART_R |= 0x00000001;            // 1) activate UART0
-  SYSCTL_RCGCGPIO_R |= 0x00000001;            // 2) activate port A
-  while((SYSCTL_PRGPIO_R & 0x0000001) == 0);
-  UART0_CTL_R &= ~0x00000001;           // 3) disable UART
-  UART0_IBRD_R = 27;                    // 4) IBRD = int(50,000,000 / (16 * 115,200)) = int(27.1267)
-  UART0_FBRD_R = 8;                     // 5) FBRD = int(0.1267 * 64 + 0.5) = 8
-  UART0_LCRH_R = (UART_LCRH_WLEN_8|UART_LCRH_FEN); // 6) 8 bit word length (no parity bits, one stop bit, FIFOs)s
-  UART0_CTL_R |= UART_CTL_UARTEN;       // 7) enable UART
-  /*--- GPIO --- */
-  GPIO_PORTA_AMSEL_R &= ~0x03;          // 8) disable analog functionality on PA
-  GPIO_PORTA_AFSEL_R |= 0x03;           // 9) enable alt funct on PA1-0
-  GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xFFFFFF00) + 0x00000011; //10) configure PA1-0 as UART
-  GPIO_PORTA_DEN_R |= 0x03;             // 11) enable digital I/O on PA1-0
+  SYSCTL_RCGCUART_R |= 0x00000001;      /* 1) Provide clock to UART0 */
+  SYSCTL_R_RCGC2_R |= 0x00000001;       /* 2) provide clock to Port A */
+  //SYSCTL_RCGCGPIO_R |= 0x00000001;       /* 2) Provide clock to Port A */
+  //SYSCTL_RCGCGPIO_R |= 0x00000020;       /* 2) Provide clock to Port A */
+  UART0_CTL_R &= ~0x00000001;           /* 3) Disable UART */
+  UART0_IBRD_R = 325;                    /* 4) Integer */
+  UART0_FBRD_R = 33;                     /* 5) Fractional */
+  UART0_LCRH_R = 0x0000070;             /* 6) 8 bit word length (no parity bits, one stop bit, FIFOs)s */
+  UART0_CTL_R |= 0x00000300;            /* 7) enable transmitter and receiver for UART0 */
+  UART0_CTL_R |= 0x00000001;            /* 8) enable UART */
+  /*--- GPIO --- */  
+  GPIO_PORTA_AMSEL_R &= ~0x03;          /* 9) disable analog functionality on PA */
+  GPIO_PORTA_AFSEL_R |= 0x03;           /* 10) enable alt funct on PA1-0 */
+  GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xFFFFFF00) + 0x00000011; /*  11) configure PA1-0 as UART */
+  GPIO_PORTA_DEN_R |= 0x03;             /* 12) enable digital I/O on PA1-0 */
 }
 
 /*
@@ -133,7 +143,7 @@ void UART0_Init(void){
 void UART5_Init(void){
     /*--- UART ---*/
     SYSCTL_RCGCUART_R |= 0x00000020; /* 1) provide clock to UART5 */
-    SYSCTL_R_RCGC2_R |= 0x00000010; /* 2) provide clock to PortE */
+    SYSCTL_R_RCGC2_R |= 0x00000010; /* 2) provide clock to Port E */
     UART5_CTL_R &= ~0x00000001; /* 3) disable UART5 during initialization */
     UART5_IBRD_R = 325; /* 4) write the integer portion to UART5_IBRD_R */
     UART5_FBRD_R = 33; /* 5) write the fractional */
@@ -156,12 +166,43 @@ unsigned char UART0_Available(void){
 
 void UART0_WriteChar(unsigned char data){
     while ((UART0_FR_R &= UART_FR_TXFF) != 0) {}
-    UART5_DR_R = data;
+    UART0_DR_R = data;
 }
 
 unsigned char UART0_ReadChar(void){
     while((UART0_FR_R &= UART_FR_RXFE) != 0) {}
     return ((unsigned char)(UART0_DR_R & 0xFF));
+}
+
+void UART0_ReadString(char *bufPtr, unsigned short maxLength){
+    char inputChar;
+    unsigned short length = 0;
+    inputChar = UART0_ReadChar();
+    while (inputChar != CR){
+        /* in case of backspace */
+        if (inputChar == BS){
+            if (length){
+                length--;
+                bufPtr--;
+                UART0_WriteChar(BS);
+            }
+        }
+        else if (length < maxLength){
+            *bufPtr = inputChar;
+            length++;
+            bufPtr++;
+            UART0_WriteChar(inputChar);
+        }
+        inputChar = UART0_ReadChar();
+    }
+    *bufPtr = '\0';
+}
+
+void UART0_WriteString(char * data){
+    while(*data != '\0'){
+        UART0_WriteChar(*data);
+        data++;
+    }
 }
 
 unsigned char UART5_Available(void){
@@ -241,7 +282,7 @@ void PortF_Init(void) {
     SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF; // 1) activate clock for GPIO PortF */
     delay = SYSCTL_RCGC2_R;
     GPIO_PORTF_AMSEL_R = 0x00; /* 2) disable analog function */
-    GPIO_PORTF_PCTL_R &= 0xFFFF000F; // 3) PCTL */
+    GPIO_PORTF_PCTL_R &= 0xFFFF000F; /* 3) PCTL */
     GPIO_PORTF_DEN_R |= 0x0E; /* 4) digital enable */
     GPIO_PORTF_DIR_R |= 0x0E; /* 5) direction */
     GPIO_PORTF_AFSEL_R &= ~0x0E; /* 6) alternate function */
@@ -258,12 +299,7 @@ void OutCRLF(int chnl){
     }
 }
 
-int main(void){
-    PLL_Init_50MHz();
-    UART5_Init();
-    UART0_Init();
-    PortF_Init();
-    GPIO_PORTF_DATA_R = 0x00;
+int UART_Comm(int num){
     UART5_WriteString("r - For Red LED");
     OutCRLF(5);
     UART5_WriteString("b - For Blue LED");
@@ -277,7 +313,7 @@ int main(void){
             unsigned char recvChar = UART5_ReadChar();
             UART5_WriteString("Received Char: ");
             UART5_WriteChar(recvChar);
-            OutCRLF();
+            OutCRLF(5);
             switch (recvChar){
                 case 'r':
                     GPIO_PORTF_DATA_R = 0x02;
@@ -292,6 +328,72 @@ int main(void){
                     GPIO_PORTF_DATA_R = 0x00;
                     break;
             }
+        }
+    }
+}
+
+int main(void){
+    PLL_Init_50MHz();
+    UART0_Init();
+    UART5_Init();
+    PortF_Init();
+    GPIO_PORTF_DATA_R = 0x00;
+    UART5_WriteString("r - For Red LED");
+    UART0_WriteString("r - For Red LED");
+    OutCRLF(5);
+    OutCRLF(0);
+    UART5_WriteString("b - For Blue LED");
+    UART0_WriteString("b - For Blue LED");
+    OutCRLF(5);
+    OutCRLF(0);
+    UART5_WriteString("g - turn Green LED");
+    UART0_WriteString("g - turn Green LED");
+    OutCRLF(5);
+    OutCRLF(0);
+    UART5_WriteString("Please input a character: ");
+    UART0_WriteString("Please input a character: ");
+    OutCRLF(5);
+    OutCRLF(0);
+    for(;;){
+        if (UART5_Available()){
+            unsigned char recvChar = UART5_ReadChar();
+            UART5_WriteString("Received Char: ");
+            UART5_WriteChar(recvChar);
+            OutCRLF(5);
+            switch (recvChar){
+                case 'r':
+                    GPIO_PORTF_DATA_R = 0x02;
+                    break;
+                case 'b':
+                    GPIO_PORTF_DATA_R = 0x04;
+                    break;
+                case 'g':
+                    GPIO_PORTF_DATA_R = 0x08;
+                    break;
+                default:
+                    GPIO_PORTF_DATA_R = 0x00;
+                    break;
+            }
+        }
+        if (UART0_Available()){
+                    unsigned char recvChar = UART0_ReadChar();
+                    UART0_WriteString("Received Char: ");
+                    UART0_WriteChar(recvChar);
+                    OutCRLF(0);
+                    switch (recvChar){
+                        case 'r':
+                            GPIO_PORTF_DATA_R = 0x02;
+                            break;
+                        case 'b':
+                            GPIO_PORTF_DATA_R = 0x04;
+                            break;
+                        case 'g':
+                            GPIO_PORTF_DATA_R = 0x08;
+                            break;
+                        default:
+                            GPIO_PORTF_DATA_R = 0x00;
+                            break;
+                    }
         }
     }
 }
