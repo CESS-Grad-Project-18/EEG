a71be23fdf6dcea423656a7691b8c8f863dc0e24
omargamal8 <omargamalh@hotmail.com>
2018-07-08 19:43:28 +0200

    added sample dataset

--- EEG/Main.py
+++ EEG/Main.py
@@ -32,57 +32,57 @@ def truncateSamples(videos, sampling_rates):
 			videos[i][ch] = videos[i][ch][:perfect]
 	print(diffs)
 
-happy_vids, sad_vids = 10, 9
-# aa_reader = Reader.AaReader()
-# happyfiles = ["Happy/"+filename for filename in os.listdir("./Happy")]
-# sadfiles = ["Sad/"+filename for filename in os.listdir("./Sad")]
-# files = happyfiles + sadfiles
-# aa_data, aa_labels = aa_reader.read([files], ["Total Labels.dat"])
+happy_vids, sad_vids = 5, 5
+aa_reader = Reader.AaReader()
+happyfiles = ["Happy/"+filename for filename in os.listdir("./Happy")]
+sadfiles = ["Sad/"+filename for filename in os.listdir("./Sad")]
+files = happyfiles + sadfiles
+aa_data, aa_labels = aa_reader.read([files], ["Total Labels.dat"])
 
-# print(aa_data.shape)
-# print(len(aa_data[0]), len(aa_data[0][0]), len(aa_data[0][0][0]))
+print(aa_data.shape)
+print(len(aa_data[0]), len(aa_data[0][0]), len(aa_data[0][0][0]))
 # # print(aa_data[0][0])
 
-# for i, video in enumerate(aa_data[0]):
-# 	print("looping video", i)
-# 	print("no channels", len(video[0]))
-# 	for j, channel in enumerate(video):
-# 		aa_data[0][i][j] = Preprocessing.car_filter(channel)
-# print(aa_data.shape, len(aa_data[0][0][0]))
+for i, video in enumerate(aa_data[0]):
+	print("looping video", i)
+	print("no channels", len(video[0]))
+	for j, channel in enumerate(video):
+		aa_data[0][i][j] = Preprocessing.car_filter(channel)
+print(aa_data.shape, len(aa_data[0][0][0]))
 
-# # # aa_data = Preprocessing.decimate_signal(aa_data)
-# # # print("--- Printing ---")
-# #
-# # #aa_data = decimate_signal(aa_data, dsType='IIR')
-# print(aa_labels)
-# video_lengths = [label[2]*60 for label in aa_labels[0]]
+# # aa_data = Preprocessing.decimate_signal(aa_data)
+# # print("--- Printing ---")
+#
+# #aa_data = decimate_signal(aa_data, dsType='IIR')
+print(aa_labels)
+video_lengths = [label[2]*60 for label in aa_labels[0]]
 
-# sampling_rates = getRates(aa_data[0], video_lengths)
-# # print(sampling_rates)
-# truncateSamples(aa_data[0], sampling_rates)
-# # # sys.exit()
-# #
-# #
-# channel_nos = [14 for _ in files]
-
-
-# processed_data, processed_labels = [], []
+sampling_rates = getRates(aa_data[0], video_lengths)
+# print(sampling_rates)
+truncateSamples(aa_data[0], sampling_rates)
+# # sys.exit()
+#
+#
+channel_nos = [14 for _ in files]
 
 
-# aa_data, aa_labels = np.asarray(aa_data), np.asarray(aa_labels)
-
-# print("aa data shape", aa_data.shape)
-# print("aa labels shape", aa_labels.shape)
-
-# for i, subject in enumerate(aa_data):
-# 	processed_data.append(Preprocessing.processReadings(subject, sampling_rates[i], video_lengths[i], channel_nos[i]))
-# 	processed_labels.append(Preprocessing.processLabels(aa_labels[i]))
+processed_data, processed_labels = [], []
 
 
-# processed_data, processed_labels = np.asarray(processed_data), np.asarray(processed_labels)
-# print(processed_data.shape, processed_labels.shape)
-# pickle.dump(processed_data, open("saved_data_truncated", "wb"))
-# pickle.dump(processed_labels, open("saved_labels_truncated", "wb"))
+aa_data, aa_labels = np.asarray(aa_data), np.asarray(aa_labels)
+
+print("aa data shape", aa_data.shape)
+print("aa labels shape", aa_labels.shape)
+
+for i, subject in enumerate(aa_data):
+	processed_data.append(Preprocessing.processReadings(subject, sampling_rates[i], video_lengths[i], channel_nos[i]))
+	processed_labels.append(Preprocessing.processLabels(aa_labels[i]))
+
+
+processed_data, processed_labels = np.asarray(processed_data), np.asarray(processed_labels)
+print(processed_data.shape, processed_labels.shape)
+pickle.dump(processed_data, open("saved_data_truncated", "wb"))
+pickle.dump(processed_labels, open("saved_labels_truncated", "wb"))
 
 
 processed_data = pickle.load(open("saved_data_truncated", "rb"))
@@ -96,7 +96,7 @@ manual_labels = [1 if i < happy_vids else 3 for i in range(happy_vids + sad_vids
 import CLFs
 total_results = None
 
-iterations = 1000
+iterations = 100
 
 for i in range(iterations):
 	s = np.arange(processed_data.shape[1])
